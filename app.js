cb6cdf20dd4df1a721b3fb0c38c3a32dfeb53be9
Tristan Gaeta <tristangaeta@gmail.com>
2021-10-13 19:09:02 -0700

    Wrote scripts for audio/visual effects

--- app.js
+++ app.js
@@ -6,6 +6,10 @@
 // vars
  var who = 'Away';
  var inning = '1';
+ const img = document.querySelector('img');
+
+ const sounds = ['buildup.mp3','charge1.mp3','charge2.mp3']
+ const images = ['megaphone.png','ups.png']
 
 //Add button listeners
 let whoButton = document.getElementById('Who');
@@ -41,4 +45,21 @@ runsDownButton.addEventListener('click',()=>{
     box = document.getElementById(who+'R')
     oldScore = parseInt(box.innerHTML);
     box.innerHTML = oldScore-1;
+});
+
+let soundButton = document.getElementById('Sound');
+soundButton.addEventListener('click',()=>{
+    let sound = sounds[Math.floor(Math.random()*sounds.length)]
+    let audio = new Audio("sounds/"+sound);
+    audio.play();
+
+    img.src = "images/"+images[Math.floor(Math.random()*images.length)]
+})
+
+let songButton = document.getElementById('Song');
+songButton.addEventListener('click',()=>{
+    let audio = new Audio("sounds/chickendance.mp3");
+    audio.play();
+
+    img.src = "images/sdchicken.jpg"
 });
